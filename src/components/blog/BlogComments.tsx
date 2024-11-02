5237587d371e9b29be96b1584e8af811d3916a7c
René Kuhm <renekuhm2@gmail.com>
2024-11-01 23:13:07 -0300

    :bug: se cambio comentarios por diskus 2

--- src/components/blog/BlogComments.tsx
+++ src/components/blog/BlogComments.tsx
@@ -1,46 +1,40 @@
 import * as React from "react";
-import { Settings } from "@/config.ts";
 
-const id = "inject-comments";
-const disqusShortname = "https-tdpblog-com-ar"; // Reemplaza con el shortname de tu sitio en Disqus
+const disqusShortname = "https-tdpblog-com-ar"; // Reemplaza con tu shortname de Disqus
 
 function BlogComments() {
-    const [mounted, setMounted] = React.useState(false);
     React.useEffect(() => {
-        setMounted(true);
-    
-        // Configuración de Disqus sin `this`
-        const disqus_config = function () {
-            const pageUrl = window.location.href; // URL única de la página
-            const pageIdentifier = window.location.pathname; // Identificador único de la página
-            return {
-                page: { url: pageUrl, identifier: pageIdentifier }
-            };
+        // Configuración de Disqus en el contexto global
+        (window as any).disqus_config = function () {
+            this.page.url = window.location.href; // URL canónica de la página
+            this.page.identifier = window.location.pathname; // Identificador único de la página
         };
-    
-        // Carga el script de Disqus
+
+        // Inserta el script de Disqus en el DOM
         const d = document;
         const s = d.createElement("script");
         s.src = `https://${disqusShortname}.disqus.com/embed.js`;
         s.setAttribute("data-timestamp", `${+new Date()}`);
         (d.head || d.body).appendChild(s);
-    
+
         return () => {
-            // Limpieza al desmontar el componente
+            // Limpieza al desmontar el componente para evitar duplicación
             if (s.parentNode) {
                 s.parentNode.removeChild(s);
             }
-            (window as any).DISQUS = undefined; // Limpia el objeto DISQUS en caso de re-montaje
+            (window as any).DISQUS = undefined; // Limpia el objeto DISQUS al desmontar
         };
     }, []);
-    
+
     return (
-        <div id={id} className="w-full">
-            {mounted ? (
-                <div id="disqus_thread"></div>
-            ) : (
-                <p>Cargando comentarios...</p>
-            )}
+        <div id="inject-comments" className="w-full">
+            <div id="disqus_thread"></div>
+            <noscript>
+                Please enable JavaScript to view the{" "}
+                <a href="https://disqus.com/?ref_noscript">
+                    comments powered by René Kuhm.
+                </a>
+            </noscript>
         </div>
     );
 }
