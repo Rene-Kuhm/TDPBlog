e8d61ff22c317c3dafd565dd6eabd4a7b6189329
René Kuhm <renekuhm2@gmail.com>
2024-11-10 17:54:05 -0300

    :bug: se cambio por commento

--- src/components/blog/BlogComments.tsx
+++ src/components/blog/BlogComments.tsx
@@ -1,20 +1,12 @@
 import * as React from "react";
 
-const disqusShortname = "https-tdpblog-com-ar"; // Reemplaza con tu shortname de Disqus
-
 function BlogComments() {
     React.useEffect(() => {
-        // Configuración de Disqus en el contexto global
-        (window as any).disqus_config = function () {
-            this.page.url = window.location.href; // URL canónica de la página
-            this.page.identifier = window.location.pathname; // Identificador único de la página
-        };
-
-        // Inserta el script de Disqus en el DOM
+        // Inserta el script de Commento en el DOM
         const d = document;
         const s = d.createElement("script");
-        s.src = `https://${disqusShortname}.disqus.com/embed.js`;
-        s.setAttribute("data-timestamp", `${+new Date()}`);
+        s.src = "https://tdpblog.com.ar/js/commento.js";
+        s.defer = true; // Utiliza defer para asegurar que el script se cargue correctamente
         (d.head || d.body).appendChild(s);
 
         return () => {
@@ -22,19 +14,13 @@ function BlogComments() {
             if (s.parentNode) {
                 s.parentNode.removeChild(s);
             }
-            (window as any).DISQUS = undefined; // Limpia el objeto DISQUS al desmontar
+            (window as any).COMMENTO = undefined; // Limpia el objeto Commento al desmontar
         };
     }, []);
 
     return (
         <div id="inject-comments" className="w-full">
-            <div id="disqus_thread"></div>
-            <noscript>
-                Please enable JavaScript to view the{" "}
-                <a href="https://disqus.com/?ref_noscript">
-                    comments powered by René Kuhm.
-                </a>
-            </noscript>
+            <div id="commento"></div>
         </div>
     );
 }
