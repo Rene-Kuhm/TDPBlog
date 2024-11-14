b769f490b02238e39cb362b56c2679b8f0379af9
René Kuhm <renekuhm2@gmail.com>
2024-11-14 16:17:05 -0300

    se modifico remark42 1

--- src/components/blog/BlogComments.tsx
+++ src/components/blog/BlogComments.tsx
@@ -2,33 +2,32 @@ import * as React from "react";
 
 function BlogComments() {
     React.useEffect(() => {
-        // Inserta el script de Commento en el DOM
+        // Inserta el script de Remark42 en el DOM
         const d = document;
         const s = d.createElement("script");
-        s.src = "https://tdpblog.com.ar/js/commento.js";
+        s.src = "https://tdpblog.com.ar/web/embed.js"; // URL del script de Remark42 en localhost
         s.async = true; // Cargar asíncronamente
-        s.defer = true; // Utiliza defer para asegurar que el script se cargue correctamente
-        (d.head || d.body).appendChild(s);
 
-        // Inicializa Commento cuando el script se carga
-        s.onload = () => {
-            if ((window as any).commento && typeof (window as any).commento.main === "function") {
-                (window as any).commento.main();
-            }
+        // Configura Remark42 antes de cargar el script
+        (window as any).remark_config = {
+            host: "https://tdpblog.com.ar", // URL del servidor Remark42
+            site_id: "tdpblog.com.ar"           // ID del sitio para Remark42
         };
+
+        (d.head || d.body).appendChild(s);
 
         return () => {
             // Limpieza al desmontar el componente para evitar duplicación
             if (s.parentNode) {
                 s.parentNode.removeChild(s);
             }
-            (window as any).COMMENTO = undefined; // Limpia el objeto Commento al desmontar
+            (window as any).remark_config = undefined; // Limpia la configuración al desmontar
         };
     }, []);
 
     return (
         <div id="inject-comments" className="w-full">
-            <div id="commento"></div>
+            <div id="remark42"></div> {/* Contenedor para los comentarios */}
         </div>
     );
 }
